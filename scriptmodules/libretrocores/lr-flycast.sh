a3965141b242d66c0efe53a90bac12ac72de4794
Tron74 <davide.duca@gmail.com>
2020-06-09 10:46:27 +0200

    Versione 1.0 Original RetroPie scripts

---
+++ scriptmodules/libretrocores/lr-flycast.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+# This file is part of The RetroPie Project
+#
+# The RetroPie Project is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
+#
+
+rp_module_id="lr-flycast"
+rp_module_desc="Dreamcast emulator - Reicast port for libretro"
+rp_module_help="Previously named lr-reicast then lr-beetle-dc\n\nDreamcast ROM Extensions: .cdi .gdi .chd, Naomi/Atomiswave ROM Extension: .zip\n\nCopy your Dreamcast/Naomi roms to $romdir/dreamcast\n\nCopy the required Dreamcast BIOS files dc_boot.bin and dc_flash.bin to $biosdir/dc\n\nCopy the required Naomi/Atomiswave BIOS files naomi.zip and awbios.zip to $biosdir/dc"
+rp_module_licence="GPL2 https://raw.githubusercontent.com/libretro/flycast/master/LICENSE"
+rp_module_section="opt"
+rp_module_flags="!mali !armv6"
+
+function depends_lr-flycast() {
+    local depends=()
+    isPlatform "videocore" && depends+=(libraspberrypi-dev)
+    isPlatform "mesa" && depends+=(libgles2-mesa-dev)
+    getDepends "${depends[@]}"
+}
+
+function _update_hook_lr-flycast() {
+    renameModule "lr-reicast" "lr-beetle-dc"
+    renameModule "lr-beetle-dc" "lr-flycast"
+}
+
+function sources_lr-flycast() {
+    gitPullOrClone "$md_build" https://github.com/libretro/flycast.git
+    # don't override our C/CXXFLAGS and set LDFLAGS to CFLAGS to avoid warnings on linking
+    applyPatch "$md_data/01_flags_fix.diff"
+}
+
+function build_lr-flycast() {
+    local params=("HAVE_LTCG=0")
+    make clean
+    if isPlatform "rpi"; then
+        if isPlatform "rpi4"; then
+            params+=("platform=rpi4")
+        elif isPlatform "mesa"; then
+            params+=("platform=rpi-mesa")
+        else
+            params+=("platform=rpi")
+        fi
+    fi
+    # temporarily disable distcc due to segfaults with cross compiler and lto
+    DISTCC_HOSTS="" make "${params[@]}"
+    md_ret_require="$md_build/flycast_libretro.so"
+}
+
+function install_lr-flycast() {
+    md_ret_files=(
+        'flycast_libretro.so'
+        'LICENSE'
+    )
+}
+
+function configure_lr-flycast() {
+    mkRomDir "dreamcast"
+    ensureSystemretroconfig "dreamcast"
+
+    mkUserDir "$biosdir/dc"
+
+    # system-specific
+    if isPlatform "gl"; then
+        iniConfig " = " "" "$configdir/dreamcast/retroarch.cfg"
+        iniSet "video_shared_context" "true"
+    fi
+
+    local def=0
+    isPlatform "kms" && def=1
+    # segfaults on the rpi without redirecting stdin from </dev/null
+    addEmulator $def "$md_id" "dreamcast" "$md_inst/flycast_libretro.so </dev/null"
+    addSystem "dreamcast"
+}
